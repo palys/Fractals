ff3f99f8c55a91558aa7581e53f2a6ba3f77cea2
Tomek <tomek201992@gmail.com>
2017-01-14 15:09:05 +0100

    Save image to file

--- src/fractals/view/FractalViewController.java
+++ src/fractals/view/FractalViewController.java
@@ -1,5 +1,11 @@
 package fractals.view;
 
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import fractals.newlogic.computing.ComputingService;
 import fractals.newlogic.computing.SingleThreadComputingService;
 import fractals.newlogic.drawing.FractalDrawer;
@@ -13,13 +19,18 @@ import fractals.newlogic.equations.MandelbrotEquationFactory;
 import fractals.newlogic.math.FloatComplex;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 
 public class FractalViewController {
 
@@ -33,7 +44,13 @@ public class FractalViewController {
 
 	private static final String ESCAPE = "Ostatnia wartoœæ";
 
-	final ComputingService computingService = new SingleThreadComputingService();
+	private final ComputingService computingService = new SingleThreadComputingService();
+
+	private final FloatComplex center = FloatComplex.ofCannonical(-0.5f, 0);
+
+	private final FloatComplex width = FloatComplex.ofReal(3);
+
+	private FractalDrawer drawer = null;
 
 	@FXML
 	private Canvas fractalCanvas;
@@ -51,6 +68,15 @@ public class FractalViewController {
 	private TextField iterationsField;
 
 	@FXML
+	private TextField saveWidth;
+
+	@FXML
+	private TextField saveHeight;
+
+	@FXML
+	private Button saveButton;
+
+	@FXML
 	private void initialize() {
 		setChooser.setItems(FXCollections.observableArrayList(MANDELBROT_SET, JULIA_SET));
 		setChooser.getSelectionModel().selectFirst();
@@ -58,28 +84,62 @@ public class FractalViewController {
 		colorChooser.setItems(FXCollections.observableArrayList(TWO_COLORS, GRADIENT, ESCAPE));
 		colorChooser.getSelectionModel().selectFirst();
 
-		iterationsField.textProperty().addListener((ChangeListener<String>) (observable, oldValue, newValue) -> {
+		configureAsNumericField(iterationsField);
+		iterationsField.setText("1000");
+
+		drawButton.setOnAction(this::draw);
+
+		configureAsNumericField(saveHeight);
+		configureAsNumericField(saveWidth);
+		saveButton.setOnAction(this::saveToFile);
+	}
+
+	private void saveToFile(ActionEvent e) {
+		final FileChooser fileChooser = new FileChooser();
+		final FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
+		fileChooser.getExtensionFilters().add(extFilter);
+
+		final File file = fileChooser.showSaveDialog(((Node) e.getSource()).getScene().getWindow());
+
+		if (file != null) {
+			try {
+				final int width = Integer.parseInt(saveWidth.getText());
+				final int height = Integer.parseInt(saveHeight.getText());
+				final WritableImage writebleImage = new WritableImage(width, height);
+				final Canvas canvas = new Canvas(width, height);
+				drawer.draw(canvas.getGraphicsContext2D(), Long.parseLong(iterationsField.getText()), center,
+						this.width);
+				canvas.snapshot(null, writebleImage);
+				final RenderedImage renderedImage = SwingFXUtils.fromFXImage(writebleImage, null);
+				ImageIO.write(renderedImage, "png", file);
+			} catch (final IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	private void draw(ActionEvent e) {
+		final long time = System.currentTimeMillis();
+
+		final long iterations = Long.parseLong(iterationsField.getText());
+
+		final String set = setChooser.getSelectionModel().getSelectedItem();
+		final String color = colorChooser.getSelectionModel().getSelectedItem();
+		final EquationFactory factory = factory(set);
+		final Colorizer colorizer = colorizer(color);
+		fractalCanvas.setWidth(((AnchorPane) fractalCanvas.getParent()).getWidth());
+		fractalCanvas.setHeight(((AnchorPane) fractalCanvas.getParent()).getHeight());
+		drawer = new FractalDrawer(colorizer, computingService, factory);
+		drawer.draw(fractalCanvas.getGraphicsContext2D(), iterations, center, width);
+
+		System.out.println("Overal: " + (System.currentTimeMillis() - time) + "ms.");
+	}
+
+	private void configureAsNumericField(TextField field) {
+		field.textProperty().addListener((ChangeListener<String>) (observable, oldValue, newValue) -> {
 			if (!newValue.matches("\\d*")) {
 				iterationsField.setText(newValue.replaceAll("[^\\d]", ""));
 			}
-		});
-		iterationsField.setText("1000");
-
-		drawButton.setOnAction(e -> {
-			final long time = System.currentTimeMillis();
-
-			final long iterations = Long.parseLong(iterationsField.getText());
-
-			final String set = setChooser.getSelectionModel().getSelectedItem();
-			final String color = colorChooser.getSelectionModel().getSelectedItem();
-			final EquationFactory factory = factory(set);
-			final Colorizer colorizer = colorizer(color);
-			fractalCanvas.setWidth(((AnchorPane) fractalCanvas.getParent()).getWidth());
-			fractalCanvas.setHeight(((AnchorPane) fractalCanvas.getParent()).getHeight());
-			new FractalDrawer(colorizer, computingService, factory).draw(fractalCanvas.getGraphicsContext2D(),
-					iterations, FloatComplex.ofCannonical(-0.5f, 0), FloatComplex.ofReal(3));
-
-			System.out.println("Overal: " + (System.currentTimeMillis() - time) + "ms.");
 		});
 	}
 
